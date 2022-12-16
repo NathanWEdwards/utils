2aea13021bd91232b60fecef2806a7045940f0fe
NathanWEdwards <Nathan.W.Edwards@Outlook.com>
2022-12-16 19:02:01 +0000

    Add identifiers file.

--- src/bin/identifiers.rs
+++ src/bin/identifiers.rs
@@ -1,38 +1,63 @@
-//! Provides a set of Ensembl gene identifiers present in a column of a flat file.
+//! Provides a set of the values present in a column of a flat file.
 
-/// Returns a list of Ensembl identifiers from a flat file (e.g. CSV).
+/// Returns a list of the values from a flat file (e.g. CSV).
 fn main() {
-    let argument_matches = clap::App::new("identifiers")
+
+    let argument_matches = cli().get_matches();
+
+    let file_argument = match argument_matches.get_one::<String>("file") {
+        Some(file) => file,
+        _ => unreachable!("required(true) prevents `None`")
+    };
+    
+    if std::path::Path::new(file_argument).exists() != true {
+        eprintln!("file error: {}", std::io::Error::from(std::io::ErrorKind::NotFound));
+        std::process::exit(1);
+    }
+
+    let no_headers_argument = argument_matches.get_flag("no_headers");
+
+    let index = match argument_matches.get_one::<String>("index") {
+        Some(index) => match index.parse::<u32>() {
+                Ok(value) => value,
+                Err(_) => {
+                        eprintln!("index error: {}", std::io::Error::from(std::io::ErrorKind::InvalidInput));
+                        std::process::exit(1);
+                }
+        },
+        _ => unreachable!("required(true) prevent `None`")
+    };
+}
+
+fn cli() -> clap::Command {
+        clap::Command::new("identifiers")
         .version("0.1.0")
         .author("Nathan Edwards <Nathan.W.Edwards@Outlook.com>")
-        .about("Return a set of Ensembl identifiers from a column present in a flat file (e.g. CSV, TSV).")
-        .arg(clap::Arg::with_name("column")
-                .short("c")
-                .long("column")
-                .takes_value(true)
-                .help("The name of the column, or row index, that possesses Ensembl identifiers."))
-        .arg(clap::Arg::with_name("delimiter")
-                .short("d")
+        .about("Return a set of identifiers from a column present in a flat file (e.g. CSV, TSV).")
+        .arg(clap::Arg::new("index")
+                .required(true)
+                .short('i')
+                .long("index")
+                .help("A column index to take the set of values."))
+        .arg(clap::Arg::new("delimiter")
+                .short('d')
                 .long("delimiter")
-                .takes_value(true)
-                .help("The delimiter character or string token that seperates each field value (e.g. \",\", \";\", '\t')."))
-        .arg(clap::Arg::with_name("file")
-                .short("f")
+                .help("The delimiter character that separates each field value (e.g. \",\", \";\", '\\t')."))
+        .arg(clap::Arg::new("file")
+                .required(true)
+                .short('f')
                 .long("file")
-                .takes_value(true)
-                .help("The flat file (e.g. CSV, TSV) file path to parse for Ensemble identifiers."))
-        .arg(clap::Arg::with_name("headers")
-                .short("h")
-                .long("headers")
-                .takes_value(true)
-                .help("States if the supplied file has a header row or not. This file expects \"yes\", \"true\", or a non-zero integer if the intended file to parse has a header. If no header is present, this file expects \"no\", \"false\", or 0 (zero)."))        
-        .get_matches()
+                .help("The flat file (e.g. CSV, TSV) file path to parse for identifiers."))
+        .arg(clap::Arg::new("no_headers")
+                .short('n')
+                .long("no-headers")
+                .action(clap::ArgAction::SetTrue)
+                .help("A flag that indicates no header row is present."))
 }
 
-
-fn read(filename: String, delimeter: Option<String>, has_headers: Option<bool>) -> Result<(), Box<dyn Error>> {
-    let mut reader = csv::ReaderBuilder::new()
-        .delimiter(delimiter.unwrap_or(',').as_bytes())
-        .has_headers(has_headers.unwrap_or(true))
-        .from_path(filename)
-}
+// fn read(filename: String, delimiter: Option<String>, has_headers: Option<bool>) -> Result<(), Box<dyn std::error::Error>> {
+//     let mut reader = csv::ReaderBuilder::new()
+//         .delimiter(*delimiter.unwrap_or(b',').as_bytes()[0])
+//         .has_headers(has_headers.unwrap_or(true))
+//         .from_path(filename);
+// }
